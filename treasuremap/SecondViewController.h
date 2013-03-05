19d391ed26e83022049974ca84650cf43e857278
Wesley Leung <Wes.k.Leung@gmail.com>
2013-03-05 15:23:05 -0800

    Initial Commit

---
+++ treasuremap/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  treasuremap
+//
+//  Created by Wesley Leung on 3/5/13.
+//  Copyright (c) 2013 Wesley Leung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
